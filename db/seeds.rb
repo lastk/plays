7eb193c0c446606aab778174328a4af51c863bc3
Rafael Oliveira <rafael.leite.oliveira@gmail.com>
2013-11-26 09:41:00 -0300

    add a couple of games to the db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+gta_url      = "http://static.gamespot.com/uploads/original/1534/15343359/2378983-gta-v-big.jpg"
+red_dead_url = "http://upload.wikimedia.org/wikipedia/pt/a/a7/Red_Dead_Redemption.jpg"
+batman_url   = "http://www.hdwallpapers.in/walls/batman_the_dark_knight_returns_part_1_movie-wide.jpg"
+today = Date.today
+current = Game.new(title: "gta v", state: "current", remote_cover: gta_url, started_on: today, url: "http://www.google.com", forum_url: "http://www.google.com")
+current.save
+
+finished1 = Game.new(title: "red dead redemption", state: "finished", remote_cover: red_dead_url, started_on: today,  url: "http://www.google.com", forum_url: "http://www.google.com")
+finished1.save
+
+finished2 = Game.new(title: "batman", state: "finished", remote_cover: batman_url, started_on: today, url: "http://www.google.com", forum_url: "http://www.google.com")
+finished2.save
